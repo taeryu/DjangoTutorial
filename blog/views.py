c370bf2539230ac8869d70726d1bc728748a6c1d
PySon <prwtaeryu@gmail.com>
2019-02-24 00:41:13 +0900

    added create/edit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 #view가 장고에서 사용자의 url요청을 받아서 해당하는 페이지를 보내는 함수 부분
 
 def post_list(request):
@@ -27,4 +29,37 @@ def post_detail(request, pk):
 
 # render는 django.shortcuts 패키지에 있는 함수로서 첫번째 파라미터로 request를, 그리고 두번째 파라미터로 템플릿을 받아들인다. 
 
-# 'post' : post dlrp anjdla
+# 'post' : post dlrp anjdla ㅇㅣ게 뭐임??
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {
+        'form' : form,
+
+    })
