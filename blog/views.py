8d14afdf3767044ff40509b48ef157e3d6cedcea
PySon <prwtaeryu@gmail.com>
2019-03-01 09:16:13 +0900

    hh

--- blog/views.py
+++ blog/views.py
@@ -45,7 +45,6 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
